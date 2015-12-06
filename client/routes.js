87b2c29faf46954fa9448ba0ac0919d427b8e2fe
Zakhid <supercomputer20001@gmail.com>
2015-12-06 12:40:55 +0600

    meteor add iron:router

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,20 @@ Router.configure({
 });
 
 Router.route('/', {name: 'home', controller: 'MainController'});
+Router.route('/tmp', {name: 'tmp1', controller: 'MapController'});
 
 MainController = RouteController.extend({
   action: function() {
   	this.render('home', {
+	    data: function () {
+	      return { posts: ['post red', 'post blue'] }
+	    }
+  	});
+  }
+});
+MapController = RouteController.extend({
+  action: function() {
+  	this.render('loading', {
 	    data: function () {
 	      return { posts: ['post red', 'post blue'] }
 	    }
