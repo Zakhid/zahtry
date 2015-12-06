e3c491452b580fb5813612b78a0613161c6f76a8
Zakhid <supercomputer20001@gmail.com>
2015-12-06 21:06:41 +0600

    renames

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,28 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
+    title: 'МЧС',
+    kind: '1',
+    descr: 'Штаб',
+    longtitude: '42.8785403',
+    latitude: '74.6242235',
+    level: '3'
   });
   
   Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
+    title: 'Главпочтамт',
+    kind: '2',
+    descr: 'Связь',
+    longtitude: '42.8746711',
+    latitude: '74.6126176',
+    level: '2'
   });
   
   Posts.insert({
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
+    title: '7этаж',
+    kind: '3',
+    descr: 'Хранилище',
+    longtitude: '42.8771752',
+    latitude: '74.600313',
+    level: '5'
   });
 }
