36d1367f285f8605a9b036c87293922c98e9fe5e
Zakhid <supercomputer20001@gmail.com>
2015-12-06 22:33:14 +0600

    good

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,8 +3,8 @@
     title: 'МЧС',
     kind: '1',
     descr: 'Штаб',
-    longtitude: '74.6242235',
     latitude: '42.8785403',
+    longtitude: '74.6242235',
     level: '3'
   });
   
@@ -12,8 +12,8 @@
     title: 'Главпочтамт',
     kind: '2',
     descr: 'Связь',
-    longtitude: '74.6126176',
     latitude: '42.8746711',
+    longtitude: '74.6126176',
     level: '2'
   });
   
@@ -21,8 +21,8 @@
     title: '7этаж',
     kind: '3',
     descr: 'Хранилище',
-    longtitude: '42.8771752',
-    latitude: '74.600313',
+    latitude: '42.8771752',
+    longtitude: '74.600313',
     level: '5'
   });
 }
